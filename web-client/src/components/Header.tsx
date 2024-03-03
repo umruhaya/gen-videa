24739b61ecea0df90b47da81b92c64d8692dfe2a
Haider Ali <110742835+H4ID3R@users.noreply.github.com>
2024-03-03 15:34:25 +0500

    Main page clear up

--- web-client/src/components/Header.tsx
+++ web-client/src/components/Header.tsx
@@ -21,12 +21,6 @@ class Header extends React.Component {
                         <i className="fas fa-cog"></i>
                     </button>
                     </a>
-                    <button className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800">
-                        Settings
-                    </button>
-                    <button className="text-white ml-4 px-3 py-1 rounded-md hover:bg-gray-700 transition-colors">
-                        <i className="fas fa-cog"></i>
-                    </button>
                 </div>
             </nav>
         );
