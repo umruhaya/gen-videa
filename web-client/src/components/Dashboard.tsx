2b991a46d984a4eddbc2e6af91cb749c80f6893e
Haider Ali <110742835+H4ID3R@users.noreply.github.com>
2024-03-24 04:01:40 +0500

    Removed previous Settings page and component

--- web-client/src/components/Dashboard.tsx
+++ web-client/src/components/Dashboard.tsx
@@ -35,11 +35,7 @@ export default function DashboardComponent() {
                         <h1 className="text-white text-2xl font-bold">View Profile</h1>
                     </div>
                 </a>
-                <a href="/settings">
-                    <div className="bg-blue-500 p-4 w-64 h-48 rounded-xl hover:scale-105">
-                        <h1 className="text-white text-2xl font-bold">View User Settings</h1>
-                    </div>
-                </a>
+
                 <div className="bg-blue-500 p-4 w-64 h-48 rounded-xl hover:scale-105">
                     <h1 className="text-white text-2xl font-bold">Video Narration</h1>
                     <em>comming soon!</em>
